2a2146495a8009f20714881929f4f87a0347bbb8
clay-bugg <kalebutler2525@gmail.com>
2025-07-20 00:29:21 +0100

    added login and logout functions to userStore.js

--- FrontEnd/stores/userStore.js
+++ FrontEnd/stores/userStore.js
@@ -22,5 +22,27 @@ const useUserStore = defineStore('user', () => {
       console.error('Signup failed:', err.error || err.message)
     }
   }
-  
+  //Login
+  async function login(username, password) { 
+    try {
+      const res = await fetch(`${apiUrl}/login`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ username, password })
+      })
+
+      if (!res.ok) throw await res.json()
+      const data = await res.json()
+      user.value = data
+      console.log('User logged in:', data)
+    } catch (err) { 
+      console.error('Login failed:', err.error || err.message)
+    }
+  }
+
+  //Logout
+  function logout() { 
+    user.value = null
+  }
+  return { user, signup, login, logout }
 })
